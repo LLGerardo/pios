056465af05b255889088f7d9072bfe387812d866
llgerardo <lauralgerardo@gmail.com>
2024-09-26 01:13:40 -0500

    decided on address

--- serial.c
+++ serial.c
@@ -4,7 +4,7 @@
 #include "rprintf.h"
 
 void putc(int data){
-	uint32_t* mu_io = (uint32_t*) //address; //set pointer to register 
+	uint32_t* mu_io = (uint32_t*) 0x3F215040; //set pointer to register 
 	*mu_io = (uint32_t)data; //write char to mu_io register
 }
 
