8e0298d9ebc4aa73a4c1aba2cd14b3babf9231d2
llgerardo <lauralgerardo@gmail.com>
2024-09-25 23:23:54 -0500

    create files for driver

--- rprintf.c
+++ rprintf.c
@@ -1,4 +1,6 @@
 #include <stdlib.io>
 #include <rprintf.h>
-
+ //bits 2 and 3 hold the exectution level and you will be able to demote yourself
+ //8 = 2 
+ // to get real exectution level do [el>>2]
 
