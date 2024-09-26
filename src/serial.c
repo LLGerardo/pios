3d7722ebaeb0c0d410f91d31631a796cb44f55dd
llgerardo <lauralgerardo@gmail.com>
2024-09-26 02:04:26 -0500

    updated putc name to avoid error

--- src/serial.c
+++ src/serial.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include "rprintf.c"
 
-void putc(int data){
+void pios_putc(int data){
 	uint32_t* mu_io = (uint32_t*) 0x3F215040; //set pointer to register 
 	*mu_io = (uint32_t)data; //write char to mu_io register
 }
