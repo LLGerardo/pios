beaf2285268120f59caea31eb2ab4749a9847972
Laura Lupita Gerardo <104596023+LLGerardo@users.noreply.github.com>
2024-09-26 06:21:33 +0000

    fixed structure and added rprintf code from class site

---
+++ src/serial.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rprintf.c"
+
+void putc(int data){
+	uint32_t* mu_io = (uint32_t*) 0x3F215040; //set pointer to register 
+	*mu_io = (uint32_t)data; //write char to mu_io register
+}
+
