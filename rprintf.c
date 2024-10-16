fc7f513ab9b9066577f36f1537a1754aaf960b9c
Laura Lupita Gerardo <104596023+LLGerardo@users.noreply.github.com>
2024-10-16 21:39:14 +0000

    add mmusetup to makefile

--- rprintf.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.io>
-#include <rprintf.h>
- //bits 2 and 3 hold the exectution level and you will be able to demote yourself
- //8 = 2 
- // to get real exectution level do [el>>2]
-
